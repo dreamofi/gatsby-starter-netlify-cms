d98887d87b349e0ccfa3b60ec409e7db05c73100
dreamofi <dxt.tfg@gmail.com>
2019-10-03 15:56:25 +0700

    test with feature list

--- src/components/Features.js
+++ src/components/Features.js
@@ -6,28 +6,37 @@ const FeatureGrid = ({gridItems}) => {
   const colorList = ['gold', 'blue', 'green', 'purple'];
   return (
     <div className="w-90 w-80-m w-70-l center pt3 flex flex-wrap mid-gray items-stretch justify-center">
-      {gridItems.map((item, index) => (
-        <div
-          key={item.text}
-          className="flex flex-wrap w-80-m w-50-l pa3 shadow-hover">
-          <section className="w-100 ba br3 b--moon-gray">
-            <div
-              className={`bg-${colorList[index]} pv1 bt b--${colorList[index]} br3 br--top`}>
-              <h2 className="tc white f5 f4-l">{item.heading}</h2>
-            </div>
-
-            <div className="tc bg-near-white">
-              <div className="db w-40 pv3 center">
-                <PreviewCompatibleImage imageInfo={item} />
+      {gridItems.map((item, index) => {
+        console.log(item);
+        return (
+          <div
+            key={item.heading}
+            className="flex flex-wrap w-80-m w-50-l pa3 shadow-hover">
+            <section className="w-100 ba br3 b--moon-gray">
+              <div
+                className={`bg-${colorList[index]} pv1 bt b--${colorList[index]} br3 br--top`}>
+                <h2 className="tc white f5 f4-l">{item.heading}</h2>
               </div>
-            </div>
 
-            <div className="f6 f5-l pa3 ph4">
-              <p>{item.text}</p>
-            </div>
-          </section>
-        </div>
-      ))}
+              <div className="tc bg-near-white">
+                <div className="db w-40 pv3 center">
+                  <PreviewCompatibleImage imageInfo={item} />
+                </div>
+              </div>
+
+              <div className="f6 f5-l pa3 ph4">
+                <ul>
+                  {item.text.map(each => (
+                    <li key={each} className="f5">
+                      {each}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </section>
+          </div>
+        );
+      })}
     </div>
   );
 };
@@ -36,7 +45,7 @@ FeatureGrid.propTypes = {
     PropTypes.shape({
       heading: PropTypes.string,
       image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-      text: PropTypes.string,
+      text: PropTypes.array,
     }),
   ),
 };
