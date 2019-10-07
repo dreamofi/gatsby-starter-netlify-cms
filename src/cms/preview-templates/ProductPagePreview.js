a6f123fc3301d487bd487d2d64a890a0b016b5fa
dreamofi <dxt.tfg@gmail.com>
2019-10-07 16:14:07 +0700

    test markdown on product page, fix preview

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {ProductPageTemplate} from '../../templates/product-page';
+import {MarkdownTemplate} from '../../templates/product-page';
 
-const ProductPagePreview = ({entry, getAsset}) => {
+const ProductPagePreview = ({entry, getAsset, widgetFor}) => {
   const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs']);
   const blurbs = entryBlurbs ? entryBlurbs.toJS() : [];
 
@@ -15,36 +16,39 @@ const ProductPagePreview = ({entry, getAsset}) => {
   // console.log(getAsset(entry.getIn(['data', 'main', 'image1', 'image'])));
 
   return (
-    <ProductPageTemplate
-      image={entry.getIn(['data', 'image'])}
-      title={entry.getIn(['data', 'title'])}
-      heading={entry.getIn(['data', 'heading'])}
-      description={entry.getIn(['data', 'description'])}
-      intro={{blurbs}}
-      main={{
-        heading: entry.getIn(['data', 'main', 'heading']),
-        description: entry.getIn(['data', 'main', 'description']),
-        image1: {
-          image: entry.getIn(['data', 'main', 'image1', 'image']),
-          alt: entry.getIn(['data', 'main', 'image1', 'alt']),
-        },
-        image2: {
-          image: entry.getIn(['data', 'main', 'image2', 'image']),
-          alt: entry.getIn(['data', 'main', 'image2', 'alt']),
-        },
-        image3: {
-          image: entry.getIn(['data', 'main', 'image3', 'image']),
-          alt: entry.getIn(['data', 'main', 'image3', 'alt']),
-        },
-      }}
-      fullImage={entry.getIn(['data', 'full_image'])}
-      testimonials={testimonials}
-      pricing={{
-        heading: entry.getIn(['data', 'pricing', 'heading']),
-        description: entry.getIn(['data', 'pricing', 'description']),
-        plans: pricingPlans,
-      }}
-    />
+    <div>
+      <ProductPageTemplate
+        image={entry.getIn(['data', 'image'])}
+        title={entry.getIn(['data', 'title'])}
+        heading={entry.getIn(['data', 'heading'])}
+        description={entry.getIn(['data', 'description'])}
+        intro={{blurbs}}
+        main={{
+          heading: entry.getIn(['data', 'main', 'heading']),
+          description: entry.getIn(['data', 'main', 'description']),
+          image1: {
+            image: entry.getIn(['data', 'main', 'image1', 'image']),
+            alt: entry.getIn(['data', 'main', 'image1', 'alt']),
+          },
+          image2: {
+            image: entry.getIn(['data', 'main', 'image2', 'image']),
+            alt: entry.getIn(['data', 'main', 'image2', 'alt']),
+          },
+          image3: {
+            image: entry.getIn(['data', 'main', 'image3', 'image']),
+            alt: entry.getIn(['data', 'main', 'image3', 'alt']),
+          },
+        }}
+        fullImage={entry.getIn(['data', 'full_image'])}
+        testimonials={testimonials}
+        pricing={{
+          heading: entry.getIn(['data', 'pricing', 'heading']),
+          description: entry.getIn(['data', 'pricing', 'description']),
+          plans: pricingPlans,
+        }}
+      />
+      <MarkdownTemplate content={widgetFor('body')} />
+    </div>
   );
 };
 
@@ -53,6 +57,7 @@ ProductPagePreview.propTypes = {
     getIn: PropTypes.func,
   }),
   getAsset: PropTypes.func,
+  widgetFor: PropTypes.func,
 };
 
 export default ProductPagePreview;
